2d5f5852d2067354e3b33dbbde9907b9e6299b1b
DanielMonoranu <92157946+DanielMonoranu@users.noreply.github.com>
2022-01-21 14:46:00 +0200

    Add files via upload

---
+++ C#/Lab 4/incauna/incauna/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace incauna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pacientiDataSet = new incauna.PacientiDataSet();
+            this.pacientiBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pacientiTableAdapter = new incauna.PacientiDataSetTableAdapters.PacientiTableAdapter();
+            this.tableAdapterManager = new incauna.PacientiDataSetTableAdapters.TableAdapterManager();
+            this.radiografiiTableAdapter = new incauna.PacientiDataSetTableAdapters.RadiografiiTableAdapter();
+            this.pacientiDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn1AdaugaRadiografie = new System.Windows.Forms.Button();
+            this.btn1Save = new System.Windows.Forms.Button();
+            this.radiografiiBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb1Cautare = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pacientiDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pacientiBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pacientiDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radiografiiBindingSource)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pacientiDataSet
+            // 
+            this.pacientiDataSet.DataSetName = "PacientiDataSet";
+            this.pacientiDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // pacientiBindingSource
+            // 
+            this.pacientiBindingSource.DataMember = "Pacienti";
+            this.pacientiBindingSource.DataSource = this.pacientiDataSet;
+            this.pacientiBindingSource.PositionChanged += new System.EventHandler(this.pacientiBindingSource_PositionChanged);
+            // 
+            // pacientiTableAdapter
+            // 
+            this.pacientiTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.PacientiTableAdapter = this.pacientiTableAdapter;
+            this.tableAdapterManager.RadiografiiTableAdapter = this.radiografiiTableAdapter;
+            this.tableAdapterManager.UpdateOrder = incauna.PacientiDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // radiografiiTableAdapter
+            // 
+            this.radiografiiTableAdapter.ClearBeforeFill = true;
+            // 
+            // pacientiDataGridView
+            // 
+            this.pacientiDataGridView.AutoGenerateColumns = false;
+            this.pacientiDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.pacientiDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.pacientiDataGridView.DataSource = this.pacientiBindingSource;
+            this.pacientiDataGridView.Location = new System.Drawing.Point(12, 67);
+            this.pacientiDataGridView.Name = "pacientiDataGridView";
+            this.pacientiDataGridView.Size = new System.Drawing.Size(300, 255);
+            this.pacientiDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "CNP";
+            this.dataGridViewTextBoxColumn1.HeaderText = "CNP";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Nume";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Nume";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Adresa";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Adresa";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(327, 30);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(137, 324);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // btn1AdaugaRadiografie
+            // 
+            this.btn1AdaugaRadiografie.Location = new System.Drawing.Point(175, 336);
+            this.btn1AdaugaRadiografie.Name = "btn1AdaugaRadiografie";
+            this.btn1AdaugaRadiografie.Size = new System.Drawing.Size(137, 23);
+            this.btn1AdaugaRadiografie.TabIndex = 7;
+            this.btn1AdaugaRadiografie.Text = "Adauga Radiografie";
+            this.btn1AdaugaRadiografie.UseVisualStyleBackColor = true;
+            this.btn1AdaugaRadiografie.Click += new System.EventHandler(this.btn1AdaugaRadiografie_Click_1);
+            // 
+            // btn1Save
+            // 
+            this.btn1Save.Location = new System.Drawing.Point(12, 336);
+            this.btn1Save.Name = "btn1Save";
+            this.btn1Save.Size = new System.Drawing.Size(117, 23);
+            this.btn1Save.TabIndex = 8;
+            this.btn1Save.Text = "Salveaza modificari";
+            this.btn1Save.UseVisualStyleBackColor = true;
+            this.btn1Save.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // radiografiiBindingSource
+            // 
+            this.radiografiiBindingSource.DataMember = "FK_Radiografii_ToTable";
+            this.radiografiiBindingSource.DataSource = this.pacientiBindingSource;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(467, 270);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(387, 89);
+            this.textBox1.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(467, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(390, 238);
+            this.panel1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Cautare dupa nume";
+            // 
+            // tb1Cautare
+            // 
+            this.tb1Cautare.Location = new System.Drawing.Point(145, 30);
+            this.tb1Cautare.Name = "tb1Cautare";
+            this.tb1Cautare.Size = new System.Drawing.Size(167, 20);
+            this.tb1Cautare.TabIndex = 12;
+            this.tb1Cautare.TextChanged += new System.EventHandler(this.tb1Cautare_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(390, 237);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 368);
+            this.Controls.Add(this.tb1Cautare);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn1AdaugaRadiografie);
+            this.Controls.Add(this.btn1Save);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.pacientiDataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pacientiDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pacientiBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pacientiDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radiografiiBindingSource)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PacientiDataSet pacientiDataSet;
+        private System.Windows.Forms.BindingSource pacientiBindingSource;
+        private PacientiDataSetTableAdapters.PacientiTableAdapter pacientiTableAdapter;
+        private PacientiDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private PacientiDataSetTableAdapters.RadiografiiTableAdapter radiografiiTableAdapter;
+        private System.Windows.Forms.DataGridView pacientiDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.BindingSource radiografiiBindingSource;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btn1AdaugaRadiografie;
+        private System.Windows.Forms.Button btn1Save;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb1Cautare;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
